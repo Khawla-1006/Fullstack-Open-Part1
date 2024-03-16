ba66b2b664601d1dc855d0ae2598286f4f7e807b
Khawla-1006 <khawla.ben.in@gmail.com>
2024-03-16 13:31:45 +0100

    Exercise1.3

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,10 +1,12 @@
 const Header = (props) =>{
+  console.log(props)
   return(
     <h1>{props.course}</h1>
   );
 }
 
 const Part = (props) =>{
+  console.log(props)
   return(
     <div>
       <p>
@@ -15,6 +17,7 @@ const Part = (props) =>{
 }
 
 const Content =(props) =>{
+  console.log(props)
   return(
     <div>  
       <Part part={props.part1} exercises={props.exercises1} />
@@ -25,6 +28,7 @@ const Content =(props) =>{
 }
 
 const Total = (props) =>{
+  console.log(props)
   return(
     <div>
       <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
@@ -34,30 +38,37 @@ const Total = (props) =>{
 
 const App = () => {
   const course = 'Half stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'state of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10 
+  } 
+  const part2 = {
+    name:'Using props to pass data',
+    exercises: 7
+  }
+  
+  const part3 = {
+    name: 'state of a component',
+    exercises: 14
+  } 
 
   return(
     <div>
       <Header course = {course} />
 
       <Content 
-      part1 = {part1} 
-      exercises1 = {exercises1} 
-      part2 = {part2}
-      exercises2 = {exercises2}
-      part3 = {part3}
-      exercises3 = {exercises3}
+      part1 = {part1.name} 
+      exercises1 = {part1.exercises} 
+      part2 = {part2.name}
+      exercises2 = {part2.exercises}
+      part3 = {part3.name}
+      exercises3 = {part3.exercises}
       />
 
       <Total 
-      exercises1={exercises1} 
-      exercises2={exercises2} 
-      exercises3={exercises3} 
+      exercises1={part1.exercises} 
+      exercises2={part2.exercises} 
+      exercises3={part3.exercises} 
       />
       
     </div>
